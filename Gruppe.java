1e1cf537bc4dba28363e9483d84b6782faa7ed70
cedrik <cedrik.meis@gmail.com>
2018-06-27 12:20:20 +0200

    push it

---
+++ Gruppe.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class Gruppe
+{
+    public ArrayList<Mannschaft>gruppe;
+    public ArrayList<Spiel>spiele;
+    public Gruppe()
+    {
+        ArrayList<Mannschaft>gruppe = new ArrayList<Mannschaft>();
+        ArrayList<Spiel>spiele = new ArrayList<Spiel>();
+    }
+}
