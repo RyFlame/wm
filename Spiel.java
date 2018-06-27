cddccfa0dd3f1277d11c8b4455a689565956b4c5
cedrik <cedrik.meis@gmail.com>
2018-06-27 13:15:52 +0200

    fsfsfwefwedhuszgfvkajsdgfwku3ngruzrtgfq

--- Spiel.java
+++ Spiel.java
@@ -2,20 +2,44 @@ import java.util.ArrayList;
 import java.util.Iterator;
 public class Spiel
 {
-    private String gespielt;
+    private boolean gespielt;
     private int heimtore;
     private int gasttore;
     private String ort;
     private double datum;
     private Mannschaft heimmannschaft;
     private Mannschaft gastmannschaft;
-    public Spiel (String ort,double datum,Mannschaft heimmannschaft,Mannschaft gastmannschaft,String gespielt)
+    public Spiel (String ort,double datum,Mannschaft heimmannschaft,Mannschaft gastmannschaft,boolean gespielt)
     {
         this.heimmannschaft = heimmannschaft;
         this.gastmannschaft = gastmannschaft;
         this.ort = ort;
         this.datum = datum;
         this.gespielt = gespielt;
+    }
+    public void setzetoreheim(int anzahl)
+    {
+        if(gespielt == false)
+        {
+            System.out.println("manipulation ist nicht erlaubt!");
+        }
+        else
+        {
+            heimtore = anzahl + heimtore;
+        }
+
+    }
+    public void setzetoregast(int anzahl)
+    {
+        if(gespielt == false)
+        {
+            System.out.println("manipulation ist nicht erlaubt!");
+        }
+        else
+        {
+            gasttore = anzahl + gasttore;
+        }
+
     }
     public int gibheimtore()
     {
