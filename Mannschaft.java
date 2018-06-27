6c6dba711e281dcd8c161ccb470d1506d7729b85
Felix <felixwelter2@gmail.com>
2018-06-27 11:54:24 +0200

    .

--- Mannschaft.java
+++ Mannschaft.java
@@ -1,33 +1,15 @@
+import java.util.ArrayList;
 
-/**
- * Beschreiben Sie hier die Klasse Mannschaft.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class Mannschaft
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Mannschaft
-     */
-    public Mannschaft()
+    private String nameDerMannschaft;
+    private ArrayList <Mannschaft> spieler; 
+    
+    public Mannschaft(String nameDerMannschaft)
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        this.nameDerMannschaft = nameDerMannschaft;
+        ArrayList<Mannschaft>spieler = new ArrayList<Mannschaft>();
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    
+    
 }
